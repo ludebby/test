72c15f84c856611b1a86e25cb41d1da6da5cce53
ludebby <z850068@gmail.com>
2019-04-26 16:59:14 +0800

    add vue project

---
+++ project/src/components/test1.vue
@@ -0,0 +1,17 @@
+
+<template>
+<div id="app">
+  <p>{{ msg }}</p>
+  <input v-model="message">
+</div>
+</template>
+<script>
+export default {
+  el: '#app',
+  data () {
+    return {
+      msg: 'Welcome to Your '
+    }
+  }
+}
+</script>
